87a2761050a5c914fbe5dbc8ba45cc899f96f0c4
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-01-16 13:17:25 +0200

    Fix extraction of tokenized text from UD; script to verify tokens equal

--- scripts/get_ud.sh
+++ scripts/get_ud.sh
@@ -11,11 +11,11 @@ cat ud/*.conllu > ud.conllu
 # Write the tokenized text of each CoNLL-U sentence to a file
 sed 's/SpaceAfter=No/_/;/^# text = /d' ud/*.conllu | udapy read.Conllu write.Conllu |
   while read line; do
-    if [[ $line == "# sent_id = "* ]]; then
-      sent_id=${line#*= }
-    elif [[ $line == "# text = "* ]]; then
-      text=${line#*= }
-      echo $text >ud/$sent_id.txt
+    if [[ ${line} == "# sent_id = "* ]]; then
+      sent_id="${line#*= }"
+    elif [[ ${line} == "# text = "* ]]; then
+      text="${line#*= }"
+      echo "${text}" > "ud/${sent_id}.txt"
       sent_id=
     fi
   done || exit 1
@@ -24,12 +24,12 @@ sed 's/SpaceAfter=No/_/;/^# text = /d' ud/*.conllu | udapy read.Conllu write.Con
 total=$(ls xml | wc -l)
 i=1
 for xml in xml/*; do
-  docid=${xml#*/}
-  docid=${docid%.xml}
-  cat ud/$docid-*.txt > ud/$docid.txt
+  doc_id="${xml#*/}"
+  doc_id="${doc_id%.xml}"
+  cat "ud/${doc_id}-"*.txt > "ud/${doc_id}.txt"
   printf "\rSplitting $xml to sentences... ($i/$total)"
   i=$((i+1))
-  python -m scripts.standard_to_sentences $xml --sentences ud/$docid.txt -o sentences_by_ud -b --suffix-format='-%04d' --suffix-start=1 2>/dev/null || exit 1
+  python -m scripts.standard_to_sentences "${xml}" --sentences "ud/${doc_id}.txt" -o sentences_by_ud -b --suffix-format='-%04d' --suffix-start=1 2>/dev/null || exit 1
 done
 echo
 echo Wrote sentences_by_ud
--- scripts/verify_tokens.sh
+++ scripts/verify_tokens.sh
@@ -1,1 +1,24 @@
 #!/usr/bin/env bash
+
+rm -f ucca_tokens.txt ud_tokens.txt
+total=$(ls xml | wc -l)
+i=1
+for xml in xml/*.xml; do
+    printf "\r$xml ($i/$total)"
+    i=$((i+1))
+    TOK_XML=$(grep -Po '(?<=text=")[^"]+' ${xml} | recode xml..unicode | tr -d '\0')
+    id=${xml#*/}
+    id=${id%.*}
+    txt=ud/${id%.*}.txt
+    TOK_UD=$(grep -o '\S\+' ${txt})
+    if [[ ${TOK_XML} != ${TOK_UD} ]]; then
+        printf "\r$xml $txt\n"
+        echo ${TOK_XML}
+        echo ${TOK_UD}
+    fi
+    echo "${id} ${TOK_XML}" | tr "\n" " " >> ucca_tokens.txt
+    echo >> ucca_tokens.txt
+    echo "${id} ${TOK_UD}" | tr "\n" " " >> ud_tokens.txt
+    echo >> ud_tokens.txt
+done
+printf "\r                              \n"
